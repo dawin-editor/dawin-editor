80c640484373c2e2af9ef9c447cd4717a3f623f6
youssuf benotsman <benotsman.youssuf@gmail.com>
2025-09-03 17:59:55 +0100

    Readme

--- src/components/Editor/content/Preview.tsx
+++ src/components/Editor/content/Preview.tsx
@@ -1,6 +1,6 @@
 import { ResizablePanel } from "@/components/ui/resizable";
 const Preview = () => {
-  return <ResizablePanel  className="bg-secondary-blue">One</ResizablePanel>;
+  return <ResizablePanel  className="bg-secondary-blue">Preview</ResizablePanel>;
 };
 
 export default Preview;
